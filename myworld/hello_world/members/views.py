e285036516efff7679958fcf43ae6f2cddf6733f
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-10-12 22:09:07 +0530

    M4: Added template and changes in setting

--- myworld/hello_world/members/views.py
+++ myworld/hello_world/members/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
+from django.template import loader
 
 def members(request):  # Name of function can be anything
-    return HttpResponse("Hello world!")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
