8a6a14b308e28761e03df2f7187848bf8e054e0f
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-10-14 02:08:46 +0530

    M8: Template, view and url changes for details page

--- myworld/hello_world/members/views.py
+++ myworld/hello_world/members/views.py
@@ -9,3 +9,11 @@ def members(request):
     'mymembers': mymembers,
   }
   return HttpResponse(template.render(context, request))
+
+def details(request, id):  #  that will deal with incoming requests to the members/details/? url
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
