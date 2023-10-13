ef7f15604536f2c14d32d6bf1140003a2bc6c088
mrityunjay27 <mrityunjay27062000@gmail.com>
2023-10-14 01:32:30 +0530

    M6: Updated model for member and applied migrations

--- myworld/hello_world/members/models.py
+++ myworld/hello_world/members/models.py
@@ -3,5 +3,7 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
+  phone = models.IntegerField(null=True)  # Because added these fields after putting some data in db, else it will through null error
+  joined_date = models.DateField(null=True)
 
 # Create your models here.
